12b6be4a408e6e73879dc2790a7314a2b3eea345
Romann BROQUE <rbroque@e1r8p9.clusters.42paris.fr>
2022-11-21 17:17:11 +0100

    mandatory part works !

--- srcs/converters/hex_conv.c
+++ srcs/converters/hex_conv.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:26:06 by rbroque           #+#    #+#             */
-/*   Updated: 2022/11/21 17:12:47 by rbroque          ###   ########.fr       */
+/*   Updated: 2022/11/21 17:16:10 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ char	*up_hex(va_list aptr)
 
 char	*address(va_list aptr)
 {
-	const unsigned int	nb = va_arg(aptr, unsigned int);
+	const void			*address = va_arg(aptr, void *);
 	char				*output;
 	char				*nb_str;
 
-	if (nb == 0)
+	if (address == NULL)
 		output = ft_strdup(NIL_DEF);
 	else
 	{
 		output = ft_strdup(PREFIX_HEX);
-		nb_str = itoa_base(nb, HEX);
+		nb_str = itoa_base((unsigned long)address, HEX);
 		output = add_str(output, nb_str, ft_strlen(nb_str));
 		free(nb_str);
 	}
